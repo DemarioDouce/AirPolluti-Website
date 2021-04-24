23ab49acce4dc7abb05d251bf298e870aee9a0a3
DemarioDouce <demario.douce567@gmail.com>
2021-04-23 21:14:36 -0400

    Created top section.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+// CSS
+import "./res/css/AppStyle.css";
+// Image
+import EarthImage from "./res/images/EarthImage.png";
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      {/* Main container */}
+      <div className="container-fluid">
+        {/* Top section */}
+        <section className="container-top text-center">
+          <div className="row">
+            {/* Header text */}
+            <div className="col-lg-6">
+              <h1 className="header-text-top">SAVE THE EARTH GOT DAMMIT</h1>
+            </div>
+            {/* Image */}
+            <div className="col-lg-6">
+              <img className="image-top" src={EarthImage} alt="Earth" />
+            </div>
+          </div>
+        </section>
+      </div>
     </>
   );
 }
