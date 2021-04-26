feafced5b2f4afa97d78f2a627c28f87602bb239
DemarioDouce <demario.douce567@gmail.com>
2021-04-25 21:02:34 -0400

    Finish up middle section.

--- src/App.js
+++ src/App.js
@@ -12,11 +12,103 @@ function App() {
           <div className="row">
             {/* Header text */}
             <div className="col-lg-6">
-              <h1 className="header-text-top">SAVE THE EARTH GOT DAMMIT</h1>
+              <h1 className="header-text-top">SAVE THE EARTH GOT DAMMIT!</h1>
             </div>
             {/* Image */}
             <div className="col-lg-6">
               <img className="image-top" src={EarthImage} alt="Earth" />
+            </div>
+          </div>
+        </section>
+        {/* Middle section */}
+        <section className="container-middle text-center">
+          <div className="row">
+            {/* First card */}
+            <div className="col-lg-4 col-sm-6">
+              <div className="card">
+                <div className="px-4 py-5">
+                  <i
+                    className="fas fa-seedling fa-5x"
+                    style={{ color: "#66BB6A" }}
+                  ></i>
+                </div>
+                <div className="px-4 pb-5">
+                  <h5>PLANT</h5>
+                  <p className="text-muted">Plant a tree.</p>
+                </div>
+              </div>
+            </div>
+            {/* Second card */}
+            <div className="col-lg-4 col-sm-6">
+              <div className="card">
+                <div className="px-4 py-5">
+                  <i
+                    class="fas fa-recycle fa-5x"
+                    style={{ color: "#1E88E5" }}
+                  ></i>
+                </div>
+                <div className="px-4 pb-5">
+                  <h5>RECYCLE</h5>
+                  <p className="text-muted">Reduce, reuse, and recycle.</p>
+                </div>
+              </div>
+            </div>
+            {/* Third card */}
+            <div className="col-lg-4 col-sm-6">
+              <div className="card">
+                <div className="px-4 py-5">
+                  <i
+                    class="fas fa-lightbulb fa-5x"
+                    style={{ color: "#FDD835" }}
+                  ></i>
+                </div>
+                <div className="px-4 pb-5">
+                  <h5>ENERGY</h5>
+                  <p className="text-muted">Use long-lasting light bulbs.</p>
+                </div>
+              </div>
+            </div>
+            {/* Fourth card */}
+            <div className="col-lg-4 col-sm-6">
+              <div className="card">
+                <div className="px-4 py-5">
+                  <i
+                    class="fas fa-shopping-basket fa-5x"
+                    style={{ color: "#00ACC1" }}
+                  ></i>
+                </div>
+                <div className="px-4 pb-5">
+                  <h5>SHOP</h5>
+                  <p className="text-muted">Shop wisely.</p>
+                </div>
+              </div>
+            </div>
+            {/* Fifth card */}
+            <div className="col-lg-4 col-sm-6">
+              <div className="card">
+                <div className="px-4 py-5">
+                  <i class="fas fa-book fa-5x" style={{ color: "#6D4C41" }}></i>
+                </div>
+                <div className="px-4 pb-5">
+                  <h5>EDUCATE</h5>
+                  <p className="text-muted">Learn more.</p>
+                </div>
+              </div>
+            </div>
+            {/* Sixth card */}
+            <div className="col-lg-4 col-sm-6">
+              <div className="card">
+                <div className="px-4 py-5">
+                  <i
+                    class="fas fa-bicycle fa-5x"
+                    style={{ color: "#E53935" }}
+                  ></i>
+                </div>
+                <div className="px-4 pb-5">
+                  <h5>BIKE</h5>
+                  <p className="text-muted">Bike more. Drive less.</p>
+                </div>
+              </div>
             </div>
           </div>
         </section>
