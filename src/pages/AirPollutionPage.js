25314fb56ee2f76d26d9cdce8984f892c69d8918
DemarioDouce <demario.douce567@gmail.com>
2021-04-28 14:04:31 -0400

    Changes and improvements.

--- src/pages/AirPollutionPage.js
+++ src/pages/AirPollutionPage.js
@@ -11,7 +11,7 @@ function AirPollution() {
   function submission(e) {
     axios
       .get(
-        "http://api.openweathermap.org/data/2.5/air_pollution?lat=50&lon=50&appid=5c5d6e30ba72c04e9d014b3a7c6c57af"
+        "http://api.openweathermap.org/data/2.5/air_pollution?lat=43.653225&lon=-79.383186&appid=5c5d6e30ba72c04e9d014b3a7c6c57af"
       )
       .then((result) => {
         console.log(result.data);
@@ -25,7 +25,41 @@ function AirPollution() {
     <>
       {/* Main container */}
       <div className="container-fluid">
-        <h1>Hello World</h1>
+        {/* Top section */}
+        <section className="container-top text-center">
+          <h1 className="header-text-top fw-bold">AIR POLLUTION DATA</h1>
+        </section>
+        {/* User input section */}
+        <section className="container-user-input text-center">
+          <form>
+            <div class="row">
+              <div class="col">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="First name"
+                  aria-label="First name"
+                />
+              </div>
+              <div class="col">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Last name"
+                  aria-label="Last name"
+                />
+              </div>
+            </div>
+            <button type="submit" class="btn">
+              SEARCH
+            </button>
+            <Link to="/" style={{ textDecoration: "none" }}>
+              <button type="submit" class="btn">
+                GO BACK
+              </button>
+            </Link>
+          </form>
+        </section>
         <FooterComponent />
       </div>
     </>
